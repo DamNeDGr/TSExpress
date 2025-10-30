def75bf0c13f36378b286e6e7e88602e6c0361ea
DamNeDGr <f89063431393@yandex.ru>
2025-10-30 10:59:34 +0400

    add cors

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import  UserRouter from './router/user'
 import AuthRouter from './router/auth'
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors())
 app.use('/api', UserRouter)
 app.use('/auth', AuthRouter)
 
